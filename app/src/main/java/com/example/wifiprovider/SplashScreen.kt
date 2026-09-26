24bd7b89eaa1a418c256e1b4f1a49cd802f6660c
unknown <saddam.jaff07@gmail.com>
2026-09-26 22:52:41 +0500

    working on splash screen

--- app/src/main/java/com/example/wifiprovider/SplashScreen.kt
+++ app/src/main/java/com/example/wifiprovider/SplashScreen.kt
@@ -1,2 +1,17 @@
 package com.example.wifiprovider
 
+import android.window.SplashScreen
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SplashScreen(){
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview(){
+    SplashScreen()
+}
