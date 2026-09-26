461053513a04789043f31bbf27897c5edd3b748f
unknown <saddam.jaff07@gmail.com>
2026-09-26 22:58:54 +0500

    working on splash screen

--- app/src/main/java/com/example/wifiprovider/SplashScreen.kt
+++ app/src/main/java/com/example/wifiprovider/SplashScreen.kt
@@ -1,11 +1,31 @@
 package com.example.wifiprovider
 
-import android.window.SplashScreen
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun SplashScreen(){
+fun SplashScreen(
+    onGetStartedClick: () -> Unit
+){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(androidx.compose.ui.graphics.Color.White),
+    ){
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) { }
+
+    }
 
 }
 
@@ -13,5 +33,5 @@ fun SplashScreen(){
 @Preview(showBackground = true)
 @Composable
 fun SplashScreenPreview(){
-    SplashScreen()
+    SplashScreen(onGetStartedClick = {})
 }
